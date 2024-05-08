cbbededb4f90851cdebdfda7319dc4b38ad2ee02
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-05-08 11:20:11 -0300

    aula 3

--- app.js
+++ app.js
@@ -10,3 +10,16 @@ app.listen(300, () => console.log('Server atuando na port 300'));
 app.get('/', (req, res) => {
     res.send(tabela2024)
 })
+
+app.get('/:sigla', (req, res) => {
+
+    const silga = req.params.sigla.toUpperCase();
+    const time = tabela2024.find(infoTime => infoTime.sigla === silga)
+
+    if( !time ) {
+        res.status(404).send('Não está na nossa lista um time com essa sigla')
+        return;
+    }
+
+    res.status(200).send(time)
+})
