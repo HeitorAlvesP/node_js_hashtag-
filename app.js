31a8e8c818956200ac22487c05419892a65b94b9
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-05-08 09:43:44 -0300

    part2

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ import tabela2024 from "./tabela.js";
 import express from 'express';
 
 
+const app = express();
+
+
+app.listen(300, () => console.log('Server atuando na port 300'));
+
+app.get('/', (req, res) => {
+    res.send(tabela2024)
+})
